a1ec475bd9a21538f1a805f67ffb87b96e4f3b88
crunderwood <calebunderwood@gmail.com>
2016-02-12 00:15:54 -0500

    Worked on ChildNode function
    I have no clue if this is what ChildNode is supposed to do so I gave it my best shot.  Right now I have it checking the state of Node n and returning the node to the left, right, above or below based on n's position(state) in the current state.  Feel free to edit or completely change it if you think its totally wrong.

--- Ai.cpp
+++ Ai.cpp
@@ -21,6 +21,51 @@ struct Node{
 //	return NULL;	
 //}
 
+Node ChildNode(Node n, string action, Node stateNode[]) {
+	Node emptyNode;
+	if (action == "left") {
+		// Checks if n is in the left column
+		if (n.state == 0 || n.state == 3 || n.state == 6) {
+			return emptyNode;
+		}
+		else {
+			// Left child of n
+			return stateNode[n.state - 1];
+		}
+	}
+	if (action == "right") {
+		// Checks if n is in the right column
+		if (n.state == 2 || n.state == 5 || n.state == 8) {
+			return emptyNode;
+		}
+		else {
+			// Right child of n
+			return stateNode[n.state + 1];
+		}
+	}
+	if (action == "up") {
+		// Checks if n is in the upper row
+		if (n.state == 0 || n.state == 1 || n.state == 2) {
+			return emptyNode;
+		}
+		else {
+			// Upper child of n
+			return stateNode[n.state - 3];
+		}
+	}
+	if (action == "down") {
+		// Checks if n is in the lower row
+		if (n.state == 6 || n.state == 7 || n.state == 8) {
+			return emptyNode;
+		}
+		else {
+			// Lower child of n
+			return stateNode[n.state + 3];
+		}
+	}
+}
+
+
 //Check to see if state is goal state	
 bool Goal_achieved(int state[]){
 	bool equal = true;
